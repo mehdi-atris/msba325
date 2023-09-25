ded184c36603a37a0b854773660d99c6709d6fd3
mehdi-atris <146013814+mehdi-atris@users.noreply.github.com>
2023-09-25 20:15:15 +0300

    Add files via upload

---
+++ Streamlit_Visualization.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+
+# Import Data for First Visualization
+data1 = pd.read_csv(r"C:\Users\PC\OneDrive - American University of Beirut\AUB\Graduate - Business Analytics (MS)\Term I - Fall 2023-2024\MSBA 325\Plotly\sales_data_sample.csv")
+# Import Data for Second Visualzation
+data2 = pd.read_csv(r"C:\Users\PC\OneDrive - American University of Beirut\AUB\Graduate - Business Analytics (MS)\Term I - Fall 2023-2024\MSBA 325\Plotly\football_stadiums.csv")
+
+# Title
+st.title('My Two Visualizations')
+# Subtitle
+st.header('Mehdi Atris')
+
+
+# Seperate Title and Subtitle from First Visualization
+st.markdown('---')
+
+
+# First Visualization
+st.subheader('First Visualization: Sales Across Countries')
+
+# Group Data by Country + Calculate Quantity Ordered in Each Country
+country_orders = data1.groupby('COUNTRY')['QUANTITYORDERED'].sum().reset_index()
+
+# Total Quantity ordered
+total_quantity_ordered = country_orders['QUANTITYORDERED'].sum()
+
+# Slider Widget: Allow Users to Set a Threshold as a Percentage of the Total Quantity Ordered
+percentage_threshold = st.slider("Threshold for Grouping Small Countries (%)", min_value=0, max_value=100, step=1, value=10, key="percentage_threshold_key")
+
+# Convert Selected Percentage Threshold to an Absolute Threshold Value
+threshold = int((percentage_threshold / 100) * total_quantity_ordered)
+
+# Function that Updates the Chart
+def update_chart(threshold):
+    # Group Smaller Countries and Sum Their Quantities
+    grouped_country_orders = country_orders.copy()
+    grouped_country_orders.loc[grouped_country_orders['QUANTITYORDERED'] < threshold, 'COUNTRY'] = 'Other Countries'
+    grouped_country_orders = grouped_country_orders.groupby('COUNTRY')['QUANTITYORDERED'].sum().reset_index()
+
+    # Pie Chart
+    fig = px.pie(grouped_country_orders, names='COUNTRY', values='QUANTITYORDERED', title='Quantity Ordered by Country')
+
+    # Show Plot
+    st.plotly_chart(fig)
+
+# Initial Chart Display
+update_chart(threshold)
+
+
+# Seperate Title and Subtitle From First Visualization
+st.markdown('---')
+
+
+# Second Visualization
+st.subheader('Second Visualization: Football Stadiums')
+
+# Group Data by Confederation and Count the Number of Stadiums in Each Confederation
+confederation_counts = data2['Confederation'].value_counts().reset_index()
+confederation_counts.columns = ['Confederation', 'Stadium Count']
+
+# Create Treemap using Plotly Express
+fig_treemap = px.treemap(confederation_counts, path=['Confederation'], values='Stadium Count',
+                         title='Number of Stadiums in Each Confederation')
+
+# Display Treemap in Streamlit
+st.plotly_chart(fig_treemap)
+
+# Allow User to Select a Confederation
+selected_confederation = st.selectbox("Select a Confederation:", confederation_counts['Confederation'].unique())
+
+# Filter Data for Selected Confederation
+selected_confederation_data = data2[data2['Confederation'] == selected_confederation]
+
+# Group Data by Country and Count Number of Stadiums in Each Country within Selected Confederation
+country_stadium_counts = selected_confederation_data['Country'].value_counts().reset_index()
+country_stadium_counts.columns = ['Country', 'Stadium Count']
+
+# Create Treemap for Stadiums in Each Country within Selected Confederation
+fig_country_treemap = px.treemap(country_stadium_counts, path=['Country'], values='Stadium Count',
+                                 title=f'Number of Stadiums in Each Country within {selected_confederation} Confederation')
+
+# Display Treemap for Stadiums in Each Country within the Selected Confederation
+st.plotly_chart(fig_country_treemap)
