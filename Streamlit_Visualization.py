22a17cc123c68f7eec1d20ce6413dfd8cb5cd06f
mehdi-atris <146013814+mehdi-atris@users.noreply.github.com>
2023-10-10 19:59:37 +0300

    Update Streamlit_Visualization.py

--- Streamlit_Visualization.py
+++ Streamlit_Visualization.py
@@ -22,9 +22,7 @@ st.markdown('---')
 
 # First Visualization
 st.subheader('First Visualization: Sales Across Countries')
-st.text("Hello! Our first Visualization here shows a pie chart of which countries a supermarket sells to.")
-st.text("You can move the slider around to group countries whose total sales to are under less than the percentage you pick!")
-
+st.text("Hello! Our first Visualization here shows a pie chart of which countries a<br>supermarket sells to. You can move the slider around to group countries whose total<br>sales to are under less than the percentage you pick!")
 # Group Data by Country + Calculate Quantity Ordered in Each Country
 country_orders = data1.groupby('COUNTRY')['QUANTITYORDERED'].sum().reset_index()
 
@@ -61,7 +59,7 @@ st.markdown('---')
 # Second Visualization
 st.subheader('Second Visualization: Football Stadiums')
 st.text("Here you can learn more about football stadiums around the world!")
-st.text("Below is a treemap showing the number of football stadiums in each Confederation shown in a treemap.")
+st.text("Below is a treemap showing the number of football stadiums in each Confederation<br>shown in a treemap.")
 
 # Group Data by Confederation and Count the Number of Stadiums in Each Confederation
 confederation_counts = data2['Confederation'].value_counts().reset_index()
